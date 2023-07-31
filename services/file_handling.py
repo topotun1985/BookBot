5522fa57ecafe7578b5652af69e18ca746b22dd6
Vadim Black <vvchernyh85@gmail.com>
2023-08-01 00:07:33 +0300

    add init database data

---
+++ services/file_handling.py
@@ -0,0 +1,18 @@
+import os
+
+BOOK_PATH = 'book/book.txt'
+PAGE_SIZE = 1050
+
+book: dict[int, str] = {}
+
+# Функция, возвращающая строку с текстом страницы и ее размер
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    pass
+
+# Функция, формирующая словарь книги
+def prepare_book(path: str) -> None:
+    pass
+
+
+# Вызов функции prepare_book для подготовки книги из текстового файла
+prepare_book(os.path.join(os.getcwd(), BOOK_PATH))
