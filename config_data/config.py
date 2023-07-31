b522ea289e7217121271e01752f2fa44d24db17c
Vadim Black <vvchernyh85@gmail.com>
2023-07-31 23:38:21 +0300

    add info about config data

--- config_data/config.py
+++ config_data/config.py
@@ -1,1 +1,19 @@
+from dataclasses import dataclass
+from environs import Env
 
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+# Создаем функцию, которая будет читать файл .env и возвращать
+# экземпляр класса Config с заполненными полями token и admin_ids
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
